88ff0e56e4010cc2bb65f599d5852d028633eecf
bxt <bxt@die-optimisten.net>
2020-10-24 22:40:43 +0000

    Deploying to gh-pages from  @ b864038b607add75a562d6823324decdd3db1277 🚀

---
+++ _next/static/a1rolNkS8_XKY6LL-ztN0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Ffb7d5399.f2eb736b18794f6a46bd.js","static\u002Fchunks\u002Fpages\u002Findex-412e7c1e1d9100c1775d.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f4f567df00ce0843964e.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
